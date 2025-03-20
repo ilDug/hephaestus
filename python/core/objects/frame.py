4db95f180ad38fee41eb68facf5fac1c2a2eaa4e
ilDug <dognini.marco@gmail.com>
2025-03-20 11:09:53 +0100

    feat: add methods for calculating restraints, loads, and displacements vectors in Frame class

--- python/core/objects/frame.py
+++ python/core/objects/frame.py
@@ -1,6 +1,6 @@
 from .beam import Beam
 from .node import Node
-
+import numpy as np
 
 class Frame:
 
@@ -32,3 +32,64 @@ class Frame:
         """Restituisce il beam con nodi n1 e n2"""
         names = [f"{n1}-{n2}", f"{n2}-{n1}"]
         return next(beam for beam in self.beams if beam.id in names)
+
+    def restraints_vector(self) -> np.ndarray:
+        """Returns the restraints matrix for the frame"""
+        return (
+            np.concatenate([node.restraints for node in self.nodes])
+            .astype(int)
+            .reshape(-1, 1)
+        )
+
+    def loads_vector(self) -> np.ndarray:
+        """Returns the loads vector for the frame"""
+        return np.concatenate([node.load_vector() for node in self.nodes]).reshape(
+            -1, 1
+        )
+
+    def global_stiffness_matrix(self) -> np.ndarray:
+        """Returns the global stiffness matrix for the frame"""
+        # calculate the size of the matrix : number of nodes * 3
+        n = len(self.nodes) * 3
+
+        # initialize the global stiffness matrix with zeros
+        K = np.zeros((n, n))
+
+        # add the stiffness matrix of each beam to the global stiffness matrix
+        for beam in self.beams:
+            i = (beam.i.id - 1) * 3
+            j = (beam.j.id - 1) * 3
+
+            # add the stiffness matrix of the beam to the global stiffness matrix
+            k_local = beam.stiffness_matrix()
+
+            K[i : i + 3, i : i + 3] += k_local[:3, :3]  # primo quadrante
+            K[j : j + 3, j : j + 3] += k_local[3:, 3:]  # quarto quadrante
+            K[i : i + 3, j : j + 3] -= k_local[:3, 3:]  # secondo quadrante
+            K[j : j + 3, i : i + 3] -= k_local[3:, :3]  # terzo quadrante
+        return K
+
+    def displacemets_vector(self) -> np.ndarray:
+        """Returns the displacements vector for the frame"""
+        # [K] x {d} = {F}
+        # {d} = [K]^-1 x {F}
+
+        # calculate the global stiffness matrix, restraints vector, and loads vector
+        K = self.global_stiffness_matrix()
+        R = self.restraints_vector()
+        F = self.loads_vector()
+
+        # setting the rows and columns of the restraints to zero
+        # and setting the diagonal to 1 to avoid singular matrix
+        # annulla le righe e le colonne della matrice di rigidezza per i nodi vincolati
+        Kr = K.copy()
+        for i in range(len(R)):
+            if R[i] == 1:
+                Kr[i, :] = 0
+                Kr[:, i] = 0
+                Kr[i, i] = 1  # Set diagonal to 1 to avoid singular matrix
+
+        # solve the equation [K] x {d} = {F} for {d}
+        D = np.linalg.solve(Kr, F)
+
+        return D
--- python/main.py
+++ python/main.py
@@ -12,16 +12,22 @@ frame = Frame()
 
 n1 = frame.add_node((0, 0))
 n2 = frame.add_node((1500, 0))
+n3 = frame.add_node((3000, 0))
 
 b1 = frame.add_beam(1, 2)
+b2 = frame.add_beam(2, 3)
 b1.set_material(E).set_section(A, Ix, Ix)
+b2.set_material(E).set_section(A, Ix, Ix)
 
 frame.node(n1).set_restraints(True, True, True)
+frame.node(n3).set_restraints(y=True)
 frame.node(n2).apply_loads(Fy=-1)
 
 for node in frame.nodes:
     print("the node is:")
     print(node.id, node.coordinates, node.restraints)
+
+np.set_printoptions(precision=3, floatmode="fixed", suppress=True)
 
 for beam in frame.beams:
     print(beam.id, beam.L, beam.E, beam.A, beam.Ix, beam.Iy)
@@ -31,67 +37,35 @@ for beam in frame.beams:
     )
     print(f"Rotation angle in π format: {angle_pi_format}")
     print(f"Rotation angle in π  fractional format: {angle_fraction_format}")
-
+    print(f"Stiffness matrix for beams `{beam.id}`:")
+    print(beam.stiffness_matrix())
 
 # #############################################################################
 
 
-np.set_printoptions(precision=3, floatmode="fixed", suppress=True)
-print("Stiffness matrix:")
-print(b1.stiffness_matrix())
-
-
-frame_restraints = (
-    np.concatenate([n1.restraints, n2.restraints]).astype(int).reshape(-1, 1)
-)
 print("Frame restraints:")
-print(frame_restraints)
+print(frame.restraints_vector())
 
-global_stiffness_matrix = b1.stiffness_matrix()
-global_stiffness_matrix_aux = global_stiffness_matrix.copy()
+print("Frame loads:")
+print(frame.loads_vector())
 
-# apply restraints
-#  setting the rows and columns of the restraints to zero
-for i, r in enumerate(frame_restraints):
-    if r == 1:
-        global_stiffness_matrix_aux[i, :] = 0
-        global_stiffness_matrix_aux[:, i] = 0
-        global_stiffness_matrix_aux[i, i] = 1
+np.set_printoptions(precision=0, floatmode="fixed", suppress=True)
+print(f"Global stiffness matrix, with size: {frame.global_stiffness_matrix().shape}")
+global_stiffness_matrix = frame.global_stiffness_matrix()
+print(global_stiffness_matrix)
 
-
-print("Global stiffness matrix aux:")
-print(global_stiffness_matrix_aux)
-det = np.linalg.det(global_stiffness_matrix_aux)
-print(f"Determinant: {det}")
-print(f"Dimensione matrice di rigidezza: {global_stiffness_matrix.shape}")
 
 np.set_printoptions(precision=5, floatmode="fixed", suppress=True)
-global_stiffness_matrix_inv = np.linalg.inv(global_stiffness_matrix_aux)
-print("Inverse global stiffness matrix:")
-print(global_stiffness_matrix_inv)
+print("displacements:")
+print(frame.displacemets_vector())
 
 
 # #############################################################################
-
-
-external_load_matrix = np.concatenate([n1.load_vector(), n2.load_vector()]).reshape(
-    -1, 1
-)
-
-# transpose the matrix to get the loads in column format
-# external_load_matrix = np.transpose(external_load_matrix)
-print("External load matrix:")
-
-print(external_load_matrix)
-# dimnsione della matrice di carico esterno
-print(f"Dimensione matrice carico esterno: {external_load_matrix.shape}")
 
 
 # [K] x {d} = {F}
 # {d} = [K]^-1 x {F}
 displacements_matrix = np.dot(global_stiffness_matrix_aux, external_load_matrix)
-print("Displacements matrix:")
-print(displacements_matrix)
 
 
 # [R] = [K] x {d} - {F}
