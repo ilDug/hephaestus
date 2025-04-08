fbfca82e130ad8d9cc60b1d20873cf5e40fd705e
ilDug <dognini.marco@gmail.com>
2025-04-08 17:55:55 +0200

    Refactor Section class and update section database
    - Converted Section class to inherit from Pydantic's BaseModel for better validation and serialization.
    - Added detailed attributes to the Section class, including geometric and mechanical properties.
    - Removed the old YAML section database and replaced it with a new CSV format for better data handling.
    - Introduced a new section database in Python that initializes HEA sections with their properties.
    - Updated the simple.py script to utilize the new HEA section database and perform calculations on section properties.

--- python/core/sections/section.py
+++ python/core/sections/section.py
@@ -1,24 +1,50 @@
-class Section:
-    """Section class
+from pydantic import BaseModel
+
+
+class Section(BaseModel):
+    """Section
     ================
     A class that represents a cross-section of a beam.
-    It contains properties such as area, moment of inertia and so on.
-    It also contains methods to calculate the section properties and to
-    generate a report of the section properties.
     """
 
-    name: str
+    profile: str
     """name of the section"""
-    A: int
-    """area of the section in mm2"""
-    Ix: int
-    """moment of inertia of the section in mm4 along the x-axis"""
-    Iy: int
-    """moment of inertia of the section in mm4 along the y-axis"""
+    g: float
+    """peso lineare del profilo in kg/m"""
 
-    def __init__(self, **properties: dict):
-        """initialize the section with the properties"""
-        self.name = properties.get("name", "generic")
-        self.A = properties.get("A", 0)
-        self.Ix = properties.get("Ix", 0)
-        self.Iy = properties.get("Iy", 0)
+    h: int
+    """height of the section in mm"""
+    b: int
+    """width of the section in mm"""
+    tw: int
+    """spessore dell'anima in mm"""
+    tf: int
+    """spessore delle flange/ali in mm"""
+    r: int
+    """raggio di raccordo in mm"""
+
+    A: int
+    """area of the section in mm^2"""
+
+    Iy: int
+    """moment of inertia of the section in mm^4 along main axis"""
+    iy: float
+    """radius of gyration of the section in mm along main axis"""
+    Wely: float
+    """Elastic section modulus of the section in mm^3 along main axis"""
+    Wply: float
+    """Plastic section modulus of the section in mm^3 along main axis"""
+
+    Iz: int
+    """moment of inertia of the section in mm^4 along the trasverse axis"""
+    iz: float
+    """radius of gyration of the section in mm along the trasverse axis"""
+    Welz: float
+    """Elastic section modulus of the section in mm^3 along the trasverse axis"""
+    Wplz: float
+    """Plastic section modulus of the section in mm^3 along the trasverse axis"""
+
+    J: int
+    """torsional constant of the section in mm^4"""
+    Cw: int
+    """Warping constant in mm6 (costante di deformazione torsionale)"""
--- python/simple.py
+++ python/simple.py
@@ -1,5 +1,6 @@
 from core.elements import Frame
 from core.materials import S235
+from core.sections.section_db import HEA_Sections
 
 # proprietà della trave: materiale e sezione
 
@@ -7,28 +8,44 @@ Ix = 3492000  # mm4 HEA100
 A = 2124  # mm2 HEA100
 
 # creo una struttura
-frame = Frame()
+# frame = Frame()
 
-n1 = frame.add_node((0, 0))
-n2 = frame.add_node((500, 0))
-n3 = frame.add_node((1000, 0))
-# n4 = frame.add_node((1000, 1000))
+# n1 = frame.add_node((0, 0))
+# n2 = frame.add_node((500, 0))
+# n3 = frame.add_node((1000, 0))
 
-b1 = frame.add_beam(n1, n2)
-b2 = frame.add_beam(n2, n3)
-# b3 = frame.add_beam(n4, n3)
-
-# b1.set_internal_releases(i=True, j=True)
-# b2.set_internal_releases(i=True, j=True)
-
-for beam in frame.beams:
-    beam.set_material(S235()).set_section(A, Ix, Ix)
+# b1 = frame.add_beam(n1, n2)
+# b2 = frame.add_beam(n2, n3)
 
 
-frame.node(n1).set_restraints(True, True, True)
-frame.node(n3).set_restraints(True, True, True)
-
-frame.node(n2).apply_loads(Fy=-100)
+# for beam in frame.beams:
+#     beam.set_material(S235()).set_section(A, Ix, Ix)
 
 
-print(frame.generate_node_report())
+# frame.node(n1).set_restraints(True, True, True)
+# frame.node(n3).set_restraints(True, True, True)
+
+# frame.node(n2).apply_loads(Fy=-100)
+
+
+# print(frame.generate_node_report()
+#
+for s in HEA_Sections:
+    s.Iy = s.Iy * 1000000
+    s.Wely = s.Wely * 1000
+    s.Welz = s.Welz * 1000
+    s.Wply = s.Wply * 1000
+    s.Wplz = s.Wplz * 1000
+    s.J = s.J * 1000
+    s.Cw = s.Cw * 1000000
+
+    s.Iy = int(s.Iy)
+    s.Wely = int(s.Wely)
+    s.Welz = int(s.Welz)
+    s.Wply = int(s.Wply)
+    s.Wplz = int(s.Wplz)
+    s.J = int(s.J)
+    s.Cw = int(s.Cw)
+
+for s in HEA_Sections:
+    print(s.model_dump_json())
