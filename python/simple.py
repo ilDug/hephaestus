71363331a0336ed408a67d47ecead363c078958b
ilDug <dognini.marco@gmail.com>
2025-04-09 23:58:05 +0200

    refactor: improve readability of beam creation by restructuring method chaining

--- python/simple.py
+++ python/simple.py
@@ -13,12 +13,19 @@ n1 = frame.add_node((0, 0))
 n2 = frame.add_node((500, 0))
 n3 = frame.add_node((1000, 0))
 
-b1 = frame.add_beam(n1, n2).set_material(m1).set_section(s1).set_side("MINOR")
+b1 = (
+    frame.add_beam(n1, n2)
+    .set_material(m1)
+    .set_section(s1)
+    .set_side("MINOR")
+    .set_internal_releases(i=True)
+)
 b2 = frame.add_beam(n2, n3).set_material(m1).set_section(s1)
 
 
 frame.node(n1).set_restraints(True, True, True)
 frame.node(n3).set_restraints(True, True, True)
+
 
 frame.node(n2).apply_loads(Fy=-100)
 
